8e31e61fa13b822e56b97a3995545e2502a6a184
Paul Bertain <paul@bertain.net>
2022-12-27 23:26:34 -0800

    fixing the message to revert to the working copy

--- lbj.sh
+++ lbj.sh
@@ -8,5 +8,5 @@ DATE_CHECK="/Users/paulb/Documents/version-control/git/lebron-watch/date-check.p
 NBA_DATA=`${NBA_PYTHON} ${NBA_SCRIPT}`
 LAKERS_GAME_CHECK=`${NBA_PYTHON} ${DATE_CHECK}`
 
-/Users/paulb/bin/imessage.sh -t GROUP -r "NBA Stats" -m "@${NBA_DATE} ${LAKERS_GAME_CHECK} And the all-time scoring update is that  ${NBA_DATA}"
+/Users/paulb/bin/imessage.sh -t GROUP -r "NBA Stats" -m "It is ${NBA_DATE} @${NBA_TIME} and ${LAKERS_GAME_CHECK} The all-time scoring update is that ${NBA_DATA}"
 
