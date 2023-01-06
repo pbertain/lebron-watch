3279937a56b3a92d6f93f0ef19c81793213db76d
Paul Bertain <paul@bertain.net>
2023-01-06 12:41:16 -0800

    cleaning out garbage, sample, old code

--- date-validate.py
+++ date-validate.py
@@ -1,34 +1,22 @@
 import datetime
 
 from nba_api.stats.static import teams
+from nba_api.stats.endpoints import leaguegamefinder
+# import only datetime class
+from datetime import datetime
 
 nba_teams = teams.get_teams()
 # Select the dictionary for the Lakers, which contains their team ID
 team = [team for team in nba_teams if team['abbreviation'] == 'LAL'][0]
 team_name = team['full_name']
 team_id = team['id']
-#print("%s have ID = %d" % (team_name, team_id))
-#nba_date = '12/26/2022'
-
-#try:
-#    datetime.datetime.strptime(nba_date,"%m/%d/%y")
-#except ValueError as err:
-#    print(err)
-
-from nba_api.stats.endpoints import leaguegamefinder
-
-# import only datetime class
-from datetime import datetime
 
 # current datetime
 now = datetime.now()
 
 current_date = now.date()
-#current_date = '2022-12-25'
-#print('Date:', current_date)
 
-
-# Query for games where the Celtics were playing
+# Query for games where the team is playing
 gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=team_id)
 # The first DataFrame of those returned is what we want.
 games = gamefinder.get_data_frames()[0]
@@ -41,30 +29,10 @@ last_team_game = games_2223.sort_values('GAME_DATE').iloc[-1]
 game_id = last_team_game.GAME_ID
 true_game_date = last_team_game.GAME_DATE
 
-#print(true_game_date)
-
-sample_date = '2022-12-26'
-
 true_date_fmt = datetime.strptime(true_game_date, '%Y-%m-%d')
-sample_date_fmt = datetime.strptime(sample_date, '%Y-%m-%d')
 
 if (true_date_fmt.date() == datetime.today().date()):
     print("TRUE")
 else:
     print("FALSE")
 
-#if (sample_date_fmt.date() == datetime.today().date()):
-#    print("Sample games match")
-#else:
-#    print("Another sample day")
-
-#print("The latest game date is %s" % game_data)
-#latest_game = games_1718[games_1718.MATCHUP.str.contains('TOR')]
-#try:
-#    latest_game = games_2223[games_2223.GAME_DATE.str.contains(current_date)]
-#except:
-#    latest_game = 'fail'
-#if (latest_game == 'fail'):
-#    print("Kareem lives on for another day")
-#else:
-#    print("Lebron is bringing it")
